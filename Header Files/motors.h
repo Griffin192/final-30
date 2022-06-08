6caa24c7572c3e918e6d1bf5ebdbf5369c2113d3
Griffin <61421048+Griffin192@users.noreply.github.com>
2022-06-08 13:33:59 -0400

    Add files via upload

--- Header Files/motors.h
+++ Header Files/motors.h
@@ -2,7 +2,8 @@
 
 #define SERVO1 0b00000001      // Servo output pin on H1
 #define SERVO2 0b00000010      // Servo output pin on H2
-#define SERVO3C 0b00000100      // Servo output pin on H3 continuos
+#define SERVO3 0b00000100
+#define SERVO4C 0b00001000      // Servo output pin on H3 continuos
 
 #define BASEr 0b01000000 //motor output right pin on H7
 #define BASEl 0b10000000 //motor output left pin on H8
